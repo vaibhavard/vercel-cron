358f658705f0d4509b73f5ea18059eb65b89e189
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-09-03 23:08:47 +0530

    Create cron.py

---
+++ api/cron.py
@@ -0,0 +1,12 @@
+import requests
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print('Hello, world!')
+        response = requests.get('https://intagpt.vercel.app')
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write(response.text.encode('utf-8'))
+        return
